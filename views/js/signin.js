e3835a663fb34857e2742e8a58e0d4e757f3dc11
yana savchenko <y.s.10.06.13@gmail.com>
2018-10-12 12:23:24 +0300

    fix form validation

--- views/js/signin.js
+++ views/js/signin.js
@@ -27,4 +27,33 @@ $(document).ready(() => {
         }
         )
     })
+    $("form").keypress((e) => {
+        if(e.keycode === 13) {
+            e.preventDefault();
+        const formData = $("form").serializeArray();
+        const user = {
+            email: formData[0].value,
+            pass: formData[1].value
+        }
+        const options = {
+            body: JSON.stringify(user),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }
+        Api.post('/auth/sign-in', options)
+        .then((res) => {
+            if (res.status == 200) {
+                return res.json();
+            }
+        })
+        .then((res) => {
+            console.log(res);
+            document.cookie = "token=" + res.token + "; path=/";
+            document.location.assign('/home');
+        }
+        )
+        }
+    })
 })
